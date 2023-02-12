8a02d1a4a6bb30cd2d023eee665a6abfe9bf48cb
Simra Sheikh <125186080+simrasheikh@users.noreply.github.com>
2023-02-12 17:13:22 +0500

    Update ISBNJava.java

--- Warmups/ISBNJava.java
+++ Warmups/ISBNJava.java
@@ -1,20 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package isbn.java;
-
 import java.util.Scanner;
-
-/**
- *
- * @author HP
- */
 public class ISBNJava {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please provide the 9-digit ISBN code for checksum:");
