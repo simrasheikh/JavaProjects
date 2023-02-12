35662401dd48c16daea798281a661d9a67212d78
Simra Sheikh <125186080+simrasheikh@users.noreply.github.com>
2023-02-12 17:16:04 +0500

    Update Swapping.java

--- Warmups/Swapping.java
+++ Warmups/Swapping.java
@@ -1,20 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package swapping;
-
 import java.util.Scanner;
-
-/**
- *
- * @author HP
- */
 public class Swapping {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
         System.out.println("Give the value for x");
