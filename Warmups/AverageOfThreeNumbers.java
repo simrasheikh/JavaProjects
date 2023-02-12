ec9d1b4936da21105587ac70b4e030a738207865
Simra Sheikh <125186080+simrasheikh@users.noreply.github.com>
2023-02-12 17:09:49 +0500

    Update AverageOfThreeNumbers.java

--- Warmups/AverageOfThreeNumbers.java
+++ Warmups/AverageOfThreeNumbers.java
@@ -1,20 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package average.of.three.numbers;
-
 import java.util.Scanner;
-
-/**
- *
- * @author HP
- */
 public class AverageOfThreeNumbers {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
        Scanner input = new Scanner(System.in);
         System.out.println("Enter 3 numbers:");
