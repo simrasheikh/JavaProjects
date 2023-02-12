fe521a898326cb6f71c5d41fcd4ac5a14a20ae13
Simra Sheikh <125186080+simrasheikh@users.noreply.github.com>
2023-02-12 16:43:43 +0500

    Update Replace.java

--- Warmups/Replace.java
+++ Warmups/Replace.java
@@ -1,28 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package replace;
-
-/**
- *
- * @author HP
- */
 public class Replace {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int first = 7;
         int second = 19;
         int third;
         third = second;
         second = first;
-        first = third;
-                                
+        first = third;               
                         
-        System.out.println("first=" + first + " second=" + second);
+        System.out.println("first= " + first + " second= " + second);
     }
     
 }
