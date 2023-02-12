35062ae5100275bacc5a7a9dab137287fa000ff3
Simra Sheikh <125186080+simrasheikh@users.noreply.github.com>
2023-02-12 17:07:19 +0500

    Update Palindrome.java

--- String/Palindrome.java
+++ String/Palindrome.java
@@ -1,21 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package palindrome;
-
 import java.util.Scanner;
-
-/**
- *
- * @author HP
- */
 public class Palindrome {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
+        
         Scanner input = new Scanner (System.in);
         System.out.println("Enter a 4 digit number and I'll do my magic and tell if it's a Plaindrome or not :3");
         
@@ -27,19 +15,19 @@ public class Palindrome {
         }
         else {
             
-        int a = num%10;
-        int b = (num%100)/10;
-        int c = (num%1000)/100;
-        int d = (num%10000)/1000;
-        
-        int reverse = (d) + (c*10) + (b*100) + (a*1000);
-        
-        if (d==a && c==b) {
-            System.out.println("The reverse of your number " + num + " is also " + reverse + ", and it is a Palindrome kudos.");
-        }
-        else {
-            System.out.println("The reverse of your number " + num + " is " + reverse + ", and it is not a Palindrome, better luck next time.");
-        }
+            int a = num%10;
+            int b = (num%100)/10;
+            int c = (num%1000)/100;
+            int d = (num%10000)/1000;
+
+            int reverse = (d) + (c*10) + (b*100) + (a*1000);
+
+            if (d==a && c==b) {
+                System.out.println("The reverse of your number " + num + " is also " + reverse + ", and it is a Palindrome kudos.");
+            }
+            else {
+                System.out.println("The reverse of your number " + num + " is " + reverse + ", and it is not a Palindrome, better luck next time.");
+            }
         }
 
     }
