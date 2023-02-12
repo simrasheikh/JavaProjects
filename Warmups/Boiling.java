d4e10b04981ed99f43d741f805c9156055fc2a7c
Simra Sheikh <125186080+simrasheikh@users.noreply.github.com>
2023-02-12 17:11:48 +0500

    Update Boiling.java

--- Warmups/Boiling.java
+++ Warmups/Boiling.java
@@ -1,20 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package boiling;
-
 import java.util.Scanner;
-
-/**
- *
- * @author HP
- */
 public class Boiling {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         System.out.println("Give in the Temperature:");
         Scanner input = new Scanner(System.in);
