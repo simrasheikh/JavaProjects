e4a6e98199c5785b31304da0afe0c2ab15ffaf2a
Simra Sheikh <125186080+simrasheikh@users.noreply.github.com>
2023-02-12 17:16:38 +0500

    Update Temperature.java

--- Warmups/Temperature.java
+++ Warmups/Temperature.java
@@ -1,29 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package temperature;
-
 import java.util.Scanner;
-
-/**
- *
- * @author HP
- */
 public class Temperature {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Type in the Celcius value:");
         Scanner input = new Scanner(System.in);
         double celcius = input.nextDouble();
         double fahrenheit = 9.0/5.0 * celcius + 32.0;
-        System.out.println("Fahrenheit is: " + fahrenheit);
-        
-       
-        
+        System.out.println("Fahrenheit is: " + fahrenheit);       
+               
     }
     
 }
