178fe79b02727b39545bf08099e0a73be7714397
Simra Sheikh <125186080+simrasheikh@users.noreply.github.com>
2023-02-12 17:12:42 +0500

    Update GCD.java

--- Warmups/GCD.java
+++ Warmups/GCD.java
@@ -1,20 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package gcd;
-
 import java.util.Scanner;
-
-/**
- *
- * @author HP
- */
 public class GCD {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
         System.out.println("Enter first positive integer");
