03de43d6d31d9461ac4149dad18af0abf815fc4a
Simra Sheikh <125186080+simrasheikh@users.noreply.github.com>
2023-02-12 17:11:22 +0500

    Update and rename Boilingandfreezing.java to BoilingAndFreezing.java

--- Warmups/BoilingAndFreezing.java
+++ Warmups/BoilingAndFreezing.java
@@ -1,21 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package boiling;
-
 import java.util.Scanner;
-
-/**
- *
- * @author HP
- */
 public class Boilingandfreezing {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        
         System.out.println("Give in the Temperature:");
         Scanner input = new Scanner(System.in);
         int temp = input.nextInt();
@@ -28,9 +16,7 @@ public class Boilingandfreezing {
                 System.out.println("The water ain't boiling oops, its in the normal mundane state");
             }
             
-        }
-                
+        } 
         
-    }
-    
+    }    
 }
