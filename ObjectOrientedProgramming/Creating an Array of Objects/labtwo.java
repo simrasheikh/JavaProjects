510d5a79108b8c97693e31251bce022557a9af2f
Simra Sheikh <125186080+simrasheikh@users.noreply.github.com>
2023-02-12 21:47:27 +0500

    Update labtwo.java

--- ObjectOrientedProgramming/Creating an Array of Objects/labtwo.java
+++ ObjectOrientedProgramming/Creating an Array of Objects/labtwo.java
@@ -1,12 +1,6 @@
 package LabTask2;
 public class labtwo {
-    public static void main(String[] args) {
-        //Problem 1
-        Book buk = new Book("Harry Potter and the Sorcerer ’s Stone", "J.K. Rowling", "1234567890", 309);
-        buk.summary();
-        
-        System.out.println("");
-        
+    public static void main(String[] args) {        
         //Problem 2
         Car[] c = new Car[3];
         c[0]= new Car("Ford", "Mustang", 2020, "Red", 4, 2);
@@ -15,13 +9,8 @@ public class labtwo {
         for (int i = 0; i < 3; i++) {
             c[i].details();
             System.out.println("");
-        }
+        }       
         
-                
-        //Problem 3
-        Pet pet = new Pet("Fido", "Labrador", 5);
-        Person person = new Person("John", 25, pet);
-        person.getDetails();                
     }
     
 }
