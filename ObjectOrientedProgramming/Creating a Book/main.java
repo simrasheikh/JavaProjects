43686d4ffc0a5c75335b20e49c092242d7aa08d5
Simra Sheikh <125186080+simrasheikh@users.noreply.github.com>
2023-02-12 21:45:26 +0500

    Update and rename labtwo.java to main.java

---
+++ ObjectOrientedProgramming/Creating a Book/main.java
@@ -0,0 +1,12 @@
+package LabTask2;
+public class labtwo {
+    public static void main(String[] args) {
+        //Problem 1
+        Book buk = new Book("Harry Potter and the Sorcerer ’s Stone", "J.K. Rowling", "1234567890", 309);
+        buk.summary();
+        
+        System.out.println("");       
+        
+    }
+    
+}
