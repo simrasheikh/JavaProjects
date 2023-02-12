f34747cf5c310a4ba4962ac9acd9b61f374ca8d3
Simra Sheikh <125186080+simrasheikh@users.noreply.github.com>
2023-02-12 17:12:16 +0500

    Update EuclideanDistance.java

--- Warmups/EuclideanDistance.java
+++ Warmups/EuclideanDistance.java
@@ -1,20 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package euclideandistance;
-
 import java.util.Scanner;
-
-/**
- *
- * @author HP
- */
 public class EuclideanDistance {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Insert the x value: ");
