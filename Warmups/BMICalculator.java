6cbff194aa7791dc813f75576321cb4253084878
Simra Sheikh <125186080+simrasheikh@users.noreply.github.com>
2023-02-12 17:10:16 +0500

    Update BMICalculator.java

--- Warmups/BMICalculator.java
+++ Warmups/BMICalculator.java
@@ -1,21 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package bmi.calculator;
-
 import java.util.Scanner;
-
-/**
- *
- * @author HP
- */
 public class BMICalculator {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+   public static void main(String[] args) {
        Scanner input = new Scanner(System.in);
         System.out.println("Enter your age:");
         int age = input.nextInt();
