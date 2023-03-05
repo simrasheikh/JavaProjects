cbc8256d7b7c6208660c0b46a3a08aa6339908af
Simra Sheikh <125186080+simrasheikh@users.noreply.github.com>
2023-03-05 18:05:51 +0500

    Update and rename Lms.java to main.java

---
+++ ObjectOrientedProgramming/Library Management System/main.java
@@ -0,0 +1,13 @@
+package lms;
+
+import java.util.Scanner;
+
+public class Lms {
+    public static void main(String[] args) {
+        
+        createAccount.ask();
+        searching.search();
+                
+    }
+    
+}
