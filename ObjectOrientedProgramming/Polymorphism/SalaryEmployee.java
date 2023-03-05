ac21f223a5540886e3992f69802639c9223b6797
Simra Sheikh <125186080+simrasheikh@users.noreply.github.com>
2023-03-05 13:21:41 +0500

    Create SalaryEmployee.java

---
+++ ObjectOrientedProgramming/Polymorphism/SalaryEmployee.java
@@ -0,0 +1,23 @@
+public class SalaryEmployee extends Employee{
+    
+    private double weeklySalary;
+    
+    SalaryEmployee(String fname, String lname, double weeklySalary) {
+        super(fname,lname);
+        this.weeklySalary = weeklySalary;       
+        
+    }
+    public double getWeeklySalary(double weeklySalary) {
+        return weeklySalary;
+    }
+    public void setWeeklySalary(double weeklySalary) {
+        weeklySalary = this.weeklySalary;
+    }
+    public double earnings() {
+        double earnings = weeklySalary * 4;
+        return earnings;
+    }
+    public String toString() {
+        return "Name: " + getFname() + " " + getLname() + "\nEarnings: " + earnings();
+    }
+}
